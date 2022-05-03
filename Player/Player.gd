2693a20b8fab484c5c6f7b8e3c5546de019925ba
James <jmes.spncer@gmail.com>
2022-05-03 15:38:06 +0100

    feat: moving on slopes

--- Player/Player.gd
+++ Player/Player.gd
@@ -11,12 +11,16 @@ onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
+var just_jumped = false
 
 func _physics_process(delta):
+	just_jumped = false
 	var input_vector = get_input_vector()
 	
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector, FRICTION)
+	update_snap_vector()
 	jump_check()
 	apply_gravity(delta)
 	update_animations(input_vector)
@@ -28,7 +32,7 @@ func apply_friction(input_vector, friction):
 		motion.x = lerp(motion.x, 0, friction)
 
 func apply_gravity(delta):
-	# if not is_on_floor():
+	if not is_on_floor():
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE)
 
@@ -46,12 +50,32 @@ func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_FORCE
+			just_jumped = true
+			snap_vector = Vector2.ZERO
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE / 2
 
 func move():
-	motion = move_and_slide(motion, Vector2.UP)
+	# This contains hacky code to get move_and_slide_with_snap working properly with slopes,
+	# to negate issues with the y-axis movement on slopes
+	# James: find a Godot tutorial that covers non-hacky platformer movement :)
+
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_position = position
+	var last_motion = motion
+
+	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+
+	# Landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+	
+	# Just left the ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0
+		position.y = last_position.y
 
 func update_animations(input_vector):
 	if input_vector.x != 0:
@@ -62,3 +86,7 @@ func update_animations(input_vector):
 	
 	if not is_on_floor():
 		spriteAnimator.play("Jump")
+
+func update_snap_vector():
+	if is_on_floor():
+		snap_vector = Vector2.DOWN
